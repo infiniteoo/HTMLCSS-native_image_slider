5770a8a3ce4dceb62e35f1ce261749d85ad91c4e
Troy Dorman <troydorman@gmail.com>
2021-06-06 09:28:53 -0600

    defined some constants. created nextslide and prevslide methods to switch between pics

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+let auto = false;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+  // get element with current class
+  const current = document.querySelector(".current");
+  // remove current class
+  current.classList.remove("current");
+  // check for next slide
+  if (current.nextElementSibling) {
+    // add current class to next sibling
+    current.nextElementSibling.classList.add("current");
+  } else {
+    // add current class to start
+    slides[0].classList.add("current");
+  }
+
+  setTimeout(() => current.classList.remove("current"));
+};
+
+const prevSlide = () => {
+  // get element with current class
+  const current = document.querySelector(".current");
+  // remove current class
+  current.classList.remove("current");
+  // check for next slide
+  if (current.previousElementSibling) {
+    // add current class to next sibling
+    current.previousElementSibling.classList.add("current");
+  } else {
+    // add current class to last
+    slides[slides.length - 1].classList.add("current");
+  }
+
+  setTimeout(() => current.classList.remove("current"));
+};
